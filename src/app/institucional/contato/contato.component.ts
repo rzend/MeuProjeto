f55059cd4a54e9714a9bd5dce16fe016181851ad
rstevan <riirezende@gmail.com>
2024-03-18 12:37:57 -0300

    roteado e configuração de menu dinamico

---
+++ src/app/institucional/contato/contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: `./contato.component.html`,
+  styles: []
+})
+export class ContatoComponent {
+
+}
---
+++ src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  standalone: true,
+  imports: [],
+  templateUrl: `./sobre.component.html`,
+  styles: []
+})
+export class SobreComponent {
+
+}

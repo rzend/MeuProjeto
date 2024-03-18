61b3592bcad0d6cf312e94a928bb923d420b5613
rstevan <riirezende@gmail.com>
2024-03-18 09:13:35 -0300

    star projeto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./navegacao/menu/menu.component";
+import { HomeComponent } from "./navegacao/home/home.component";
+import { FooterComponent } from "./navegacao/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [RouterOutlet, MenuComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'MeuProjeto';

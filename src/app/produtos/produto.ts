6146f5fb1871ef4585beeeaac20c6a9b6fa4b92a
rstevan <riirezende@gmail.com>
2024-03-18 18:12:09 -0300

    consumindo dados

---
+++ src/app/produtos/produto.ts
@@ -0,0 +1,8 @@
+export class Produto {
+    id: string | undefined;
+    nome: string | undefined;
+    valor: string | undefined;
+    promocao: boolean | undefined;
+    valorPromo: string | undefined;
+    imagem: string | undefined;    
+}
